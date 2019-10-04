e01596a555ef05398b7d00a5be18b01c5684b918
Alexander Robles <alexander.robles@ibm.com>
2019-10-04 11:46:48 -0300

    Add command2

---
+++ command2.py
@@ -0,0 +1,12 @@
+import click
+
+@click.group()
+def test2_name():
+    pass
+
+@test2_name.command()
+@click.option('--parameter-second', default='This is the default value',
+              help='This is the help message for command 2')
+def test(parameter_second):
+    # Do something with the parameter
+    click.echo(parameter_second)
